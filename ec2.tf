ca2e9453ee81ca503441c9ebab938e7f4fde6563
Akash <phalke.aka@gmail.com>
2023-01-10 17:37:22 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = ">= 1.2.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "4.20"
+      source = "hashicorp/aws"
+      version = "4.49.0"
     }
   }
 }
+
 provider "aws" {
   region  = "us-east-1"
 }
