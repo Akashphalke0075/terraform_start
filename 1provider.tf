d39cb43814793589bc25649efb62db8b8a815a71
Akash <phalke.aka@gmail.com>
2023-01-31 17:52:08 +0530

    update

--- 1provider.tf
+++ 1provider.tf
@@ -1,22 +1,22 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+# # Configure the AWS Provider
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0b5eea76982371e91"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "ami-0b5eea76982371e91"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "my first instance"
-  }
-}
+#   tags = {
+#     Name = "my first instance"
+#   }
+# }
---
+++ 2.provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.52.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-0b5eea76982371e91"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "work-new"
+    }
+}
