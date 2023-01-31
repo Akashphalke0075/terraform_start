af320e32bb0c6bcae35b169a45b378653348bb01
Akash <phalke.aka@gmail.com>
2023-01-31 19:14:11 +0530

    update

--- 3.s3.tf
+++ 3.s3.tf
@@ -1,5 +1,8 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "s3-website-test.hashicorp.com"
-  acl    = "public-read"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
 
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
